61187cd5e09cf3046ba057826212cfb509a93c35
Utsiboy <utsavsen46@gmail.com>
2017-12-15 19:03:15 +0530

    first commit

---
+++ gulp.sh
@@ -0,0 +1,26 @@
+
+
+sudo npm install gulp --save
+sudo npm install gulp-util --save
+sudo npm install gulp-minify --save
+sudo npm install jshint --save
+sudo npm install jshint-stylish --save
+sudo npm install gulp-jshint --save
+sudo npm install gulp-rename --save
+sudo npm install gulp-concat --save
+sudo npm install gulp-uglifycss --save
+sudo npm install gulp-clean --save
+sudo npm install gulp-compass --save
+sudo npm install gulp-connect --save
+sudo npm install del --save
+sudo npm install run-sequence --save
+
+gem sudo install --local compass
+gem sudo install --local sass
+
+bower install angularjs  --save
+bower install angular-material  --save
+bower install angular-route  --save
+bower install angular-scroll  --save
+bower install angular-carousel  --save
+bower install angular-scroll-animate  --save
